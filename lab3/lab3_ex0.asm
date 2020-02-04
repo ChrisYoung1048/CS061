3d8ea7c75f9c916052b38ad405d91fa07e8dd484
Christopher Young <chrisyoung1048@gmail.com>
2020-02-04 14:04:54 -0800

    Add files via upload

---
+++ lab3/lab3_ex0.asm
@@ -0,0 +1,21 @@
+;=================================================
+; Name: Christopher Young
+; Email: chrisyoung1048@gmail.com
+; 
+; Lab: lab 3, ex 0
+; Lab section: 025
+; TA: Jang-Shin Lin
+; 
+;=================================================
+
+.ORIG x3000 
+
+;Instruction
+GETC
+OUT
+
+HALT
+
+;Local Data
+
+.END
